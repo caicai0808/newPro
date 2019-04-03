a470d098fd992f3b058ef617820c1df51876f5de
songcaiying <songcaiying@58ganji.com>
2019-04-03 17:06:35 +0800

    ip change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const host = 'http://140.143.227.92:8010'
+const host = 'http://62.234.155.98'
 export default {
   'members':`${host}/vote/members`,//查看候选⼈信息
   'vsubmit':`${host}/vote/submit`,//投票,
