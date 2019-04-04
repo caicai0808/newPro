3321367747655133402be8bbf1a3a2a12373b2ee
caiyingsong <1170059269@qq.com>
2019-04-04 17:32:02 +0800

    Update api.js
    ip更好

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const host = 'http://39.96.216.61:8848'
+const host = 'http://39.106.124.170'
 export default {
   'members':`${host}/vote/members`,//查看候选⼈信息
   'vsubmit':`${host}/vote/submit`,//投票,
